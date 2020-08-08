e809afea420a5b2462f7ae75cce3249dbd5e21c9
SanjayRajR <reachsanjayraj@gmail.com>
2020-08-08 13:04:16 +0530

    Removed node modules

---
+++ Frontend/src/app/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+
+// export const appRoutes: Routes = [
+//     {
+//         path: 'signup', component: SignUpComponent,
+//     },
+//     {
+//         path: 'signin', component: SignInComponent,
+//     },
+//     {
+//         path: '', redirectTo: '/signin', pathMatch: 'full'
+//     }
+// ];
