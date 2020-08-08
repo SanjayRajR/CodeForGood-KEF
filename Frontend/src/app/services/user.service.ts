e9370534b1d10c587e8b7be2a9f64d9758cafb7e
SanjayRajR <reachsanjayraj@gmail.com>
2020-08-08 16:36:44 +0530

    Sign up working

---
+++ Frontend/src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  //Http Methods
+
+  postUser(user){
+    console.log(user);
+    return this.http.post(environment.apiBaseUrl + '/signup', user);
+  }
+
+
+
+}
