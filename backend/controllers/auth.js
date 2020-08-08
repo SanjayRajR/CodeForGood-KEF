3af47f9ec217250cd27e476e2291c40bdf9e162e
mohithb987 <mohithb987@gmail.com>
2020-08-08 22:49:50 +0530

    changed

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,6 +4,7 @@ const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
+    console.log(req.body);
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
